738a441ec49494753f6d839547ab1fdfc7aeb274
tejaswini0531 <dtejaswini3105@gmail.com>
2026-06-08 18:54:31 +0530

    after-routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
 // import { useState } from "react"
-import Doctorcard from "./components/Doctorcard"
 import Navbar from "./components/Navbar"
 import Section from "./components/Section"
 import './components/styles.css'
 import Addnewdoctor from "./components/Addnewdoctor"
-// import { useEffect } from "react"
+import { Route, Routes } from "react-router-dom"
 
 function App(){
-  // let [count,setCount]=useState(0)
-
-  //   console.log('comp is')
-  //   console.log('running')
-
-  // function inccount(){
-  //   setCount(count+1)
-  //   console.log(count)
-  // }
-
-  // useEffect(()=>{
-  //   console.log('useeffect is running')
-  // },[count])
   return (
    <div>
-    {/* {count}
-    <button onClick={inccount}>click to increase</button> */}
     <Navbar/>
-    <Section/>
-    <div className='doctorparent'>
-      <Doctorcard name='Sivaram' specialization='Bones' gender='male' salary={600000} />
-      <Doctorcard name='Teja' specialization='Muscles' gender='male' salary={700000} />
-      <Doctorcard name='Vasavi' specialization='Psycho' gender='female' salary={800000} />
-      <Doctorcard name='Madhu' specialization='Orthocare' gender='male' salary={500000} />
-      <Doctorcard name='Aditya' specialization='Skin specialist' gender='male' salary={900000} />
-      <Doctorcard name='John' specialization='Lungs' gender='male' salary={750000} />
-    </div>
-
-    {/* call here */}
-      <Addnewdoctor/>
+    <Routes>
+      <Route path='/' element={<section/>}/>
+      <Route path='/add-doctor' element={<Addnewdoctor/>}/>
+    </Routes>
    </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function Navbar() {
+  let navigate=useNavigate()
   return (
     <header>
         <h1>Doctor app</h1>
         <div>
-            <button>Home</button>
-            <button>Add doctor</button>
+            <button onClick={()=>navigate('/')}>Home</button>
+            <button onClick={()=>navigate('/add-doctor')}>Home</button>
+
         </div>
     </header>
   )
 }
-
 export default Navbar
 
 
